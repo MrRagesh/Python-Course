2939a01024cb0f73c132984619bb9e09489b6008
Ragesh-iTech <ragesh12082005@gmail.com>
2026-07-16 10:20:19 +0530

    to learning pandas library implementing data sets

--- 04_Pandas_Library_Deep_Dive/pandas_1.py
+++ 04_Pandas_Library_Deep_Dive/pandas_1.py
@@ -1,9 +1,36 @@
 """
 Chapter 1: getting & knowing your data
+
+- meaning full insites -> the information don't know the client to find out the data analysis to tell them. so
+he can take decision easy.
 """
 
 import pandas as pd
 
-demo = pd.read_csv(filepath_or_buffer='data.tsv.txt', sep='\t')
 
+order_detailes= pd.read_csv(filepath_or_buffer='data.tsv.txt', sep='\t')
+print(order_detailes)
+
+# 1.1 Perform Initial Analysis
+#Function - ()
+#Attribute - no gives value (shape) is a attribute
+
+shape_data = order_detailes.shape
+print(shape_data)
+
+# Terminology Alert
+    # Rows - Obsservation/Record
+    # Column - Features/Parameters
+
+null_entery = order_detailes.isnull().sum() # Chain of functions, chain of commands
+print(null_entery)
+
+"""demo = pd.read_table(filepath_or_buffer="/Users/ragesh/Documents/Entire Projects/Python Course/02_Python_Libraries_introduction/insurance.csv", sep=",")
 print(demo)
+
+d1 = pd.read_table(filepath_or_buffer="data.tsv.txt")
+print(d1)
+
+"""
+
+
