aa8a7ccadca19e8a831fa3ca7c84c553634f9cb7
Ragesh-iTech <ragesh12082005@gmail.com>
2026-07-26 21:20:03 +0530

    assign the simple task

---
+++ 06_Data_Visualization/task.py
@@ -0,0 +1,9 @@
+"""
+Task
+
+1. perform all types of data visualization analysis on this titanic dataset
+2. fing out discrete vs continous variable from this
+3. plot all the charts mentioned in the screenshot
+
+updata in the comment section
+"""
