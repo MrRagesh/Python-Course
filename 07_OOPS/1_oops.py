2a29296583484e663dc6e1e4cc3dc46354a5d4a8
Ragesh-iTech <ragesh12082005@gmail.com>
2026-07-28 11:38:33 +0530

    learning the library creation

---
+++ 07_OOPS/1_oops.py
@@ -0,0 +1,5 @@
+"""
+1. How to create our own module & import it like Pandas library?
+2. Creating Modules VS Purpose of OOPS
+3. How to create class, objects and inheritance methods and it's real-time purpose?
+"""
---
+++ 07_OOPS/Ragesh_Library/pandas_math.py
@@ -0,0 +1,25 @@
+
+"""
+Terminology Alert
+------------------
+* parameters - while creating the function, you create parameters
+* attributes - while calling the function, you create arguments
+
+Difference between return and print
+
+return - impacts the original data when the function is called
+print - if wont impact the original data, but just prints
+"""
+
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(x,y):
+    return x*y
+
+print(addition(3,4))
+print(subtraction(3,4))
+print(multiplication(3,4))
