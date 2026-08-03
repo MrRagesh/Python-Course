3e95049ad0c7d8e984298ae80b567c6ab3a5e9e7
Ragesh-iTech <ragesh12082005@gmail.com>
2026-08-03 11:49:10 +0530

    contet

--- 07_OOPS/3_oops.py
+++ 07_OOPS/3_oops.py
@@ -1,3 +1,29 @@
 """
 3. how to create class , objects and inheritance methods and it's real-time purpose?
+
+real-world analogy 1:
+
+I want to reuse the same function for team members and managers, but they have small differences.
+how do I do that?
+
+both team members and managers should be able to log in, but
+- team members -> should log in, view task, and submit tasks.
+- managers -> should log in, view task, and assign task
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
